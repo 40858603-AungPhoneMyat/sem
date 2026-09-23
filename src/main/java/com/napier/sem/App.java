dd4cb0434ec564825f5689bd0d27aa91062ad8c7
40858603-AungPhoneMyat <40858603@live.napier.ac.uk>
2026-09-23 11:20:01 +0630

    App

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,24 +9,29 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-        // Connect to MongoDB
-//        MongoClient mongoClient = new MongoClient("mongodb-server");
-        // Get a database - will create when we use it
+        // Connect to MongoDB container
+        MongoClient mongoClient = new MongoClient("mongo-dbserver", 27017);
+
+        // Get a database - will be created when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
+
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
+
         // Create a document to store
         Document doc = new Document("name", "Kevin Sim")
                 .append("class", "DevOps")
                 .append("year", "2024")
                 .append("result", new Document("CW", 95).append("EX", 85));
+
         // Add document to collection
         collection.insertOne(doc);
 
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
+
+        // Close connection
+        mongoClient.close();
     }
 }
